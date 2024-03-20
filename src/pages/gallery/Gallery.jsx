04c93a962b41401bac2ddb99d981255d08242526
hvacTech0302 <mb030287@proton.me>
2024-03-20 11:54:30 -0400

    Gallery and NotFound Pages complete with minimal styling on the NotFound Page. Need to research and fix container overflow on gallery images where the image border radius and the container border radius are different. Added TODO.

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -3,13 +3,32 @@ import HeaderImage from "../../images/header_bg_3.jpg";
 import "./gallery.css";
 
 const Gallery = () => {
+  const galleryLength = 15;
+
+  const images = [];
+
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+  console.log(images);
   return (
     <>
-      <Header title="Gallery" image={HeaderImage}>
+      <Header title="Our Gallery" image={HeaderImage}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro aliquam
         deleniti obcaecati omnis? Beatae nam unde aperiam facilis ipsa
         molestiae?
       </Header>
+      <section className="gallery">
+        <div className="container gallery-container">
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery ${index + 1}`} />
+              </article>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 };
