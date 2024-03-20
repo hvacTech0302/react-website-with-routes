efa0ea8a80df3513b427234dd0ab792de41ccc87
hvacTech0302 <mb030287@proton.me>
2024-03-20 12:42:46 -0400

    Plans page completed and styled.

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,15 +1,40 @@
+import { plans } from "../../data";
 import Header from "../../components/Header";
 import HeaderImage from "../../images/header_bg_4.jpg";
+import Card from "../../components/Card";
 import "./plans.css";
 
 const Plans = () => {
   return (
     <>
-      <Header title="Plans" image={HeaderImage}>
+      <Header title="Membership Plans" image={HeaderImage}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro aliquam
         deleniti obcaecati omnis? Beatae nam unde aperiam facilis ipsa
         molestiae?
       </Header>
+      <section className="plans">
+        <div className="container plans-container">
+          {plans.map(({ id, name, desc, price, features }) => {
+            return (
+              <Card key={id} className="plan">
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>{`$${price}`}</h1>
+                <h2>/mo</h2>
+                <h4>Features</h4>
+                {features.map(({ feature, available }, index) => {
+                  return (
+                    <p key={index} className={!available ? "disabled" : ""}>
+                      {feature}
+                    </p>
+                  );
+                })}
+                <button className="btn lg">Choose Plan</button>
+              </Card>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 };
