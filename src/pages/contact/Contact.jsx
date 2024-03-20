c3897b5564cbafd04a4c540f98a713fdebd923b6
hvacTech0302 <mb030287@proton.me>
2024-03-20 10:17:27 -0400

    Complete with Contacts Page. Styling and Media Queries complete. Only changing styling on small devices <600px for this page. On to Gallery.

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,15 +1,45 @@
 import Header from "../../components/Header";
 import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 import "./contact.css";
 
 const Contact = () => {
   return (
     <>
-      <Header title="Contact Us" image={HeaderImage}>
+      <Header title="Get In Touch" image={HeaderImage}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro aliquam
         deleniti obcaecati omnis? Beatae nam unde aperiam facilis ipsa
         molestiae?
       </Header>
+      <section className="contact">
+        <div className="container contact-container">
+          <div className="contact-wrapper">
+            <a
+              href="mailto:test.email@email.org"
+              target="_blank"
+              rel="norefferer noopener"
+            >
+              <MdEmail />
+            </a>
+            <a
+              href="https://m.me/test-messenger"
+              target="_blank"
+              rel="norefferer noopener"
+            >
+              <BsMessenger />
+            </a>
+            <a
+              href="https://wa.me/+12345678901"
+              target="_blank"
+              rel="norefferer noopener"
+            >
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
